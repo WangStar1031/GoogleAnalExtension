3b9d3bf84c91de566e4e7a1ba6519d2ac72fa7ff
Wang Xing <wangstar1031@hotmail.com>
2019-06-27 11:20:08 +0800

    For mike code.

--- js/process.js
+++ js/process.js
@@ -5,12 +5,6 @@ strScript += '  function gtag(){dataLayer.push(arguments);}';
 strScript += '  gtag("js", new Date());';
 strScript += '  gtag("config", "UA-142671532-1");';
 
-// Me
-// strScript += '  window.dataLayer = window.dataLayer || [];';
-// strScript += '  function gtag(){dataLayer.push(arguments);}';
-// strScript += '  gtag("js", new Date());';
-// strScript += '  gtag("config", "UA-142847578-1");';
-
 var timer = setInterval(function(){
 	var head = document.getElementsByTagName("head");
 	if( head.length ){
@@ -20,20 +14,10 @@ var timer = setInterval(function(){
 		var s = document.createElement('script');
 		// Mike
 		s.src = "https://www.googletagmanager.com/gtag/js?id=UA-142671532-1";
-
-		// Me
-		// s.src = "https://www.googletagmanager.com/gtag/js?id=UA-142847578-1";
-
 		s.setAttribute("async", "");
-		// s.type="text/javascript";
-		// s.innerHTML = strScript;
 		document.head.appendChild(s);
 		var func = document.createElement('script');
 		func.innerHTML = strScript;
 		document.head.appendChild(func);
-
-		// chrome.tabs.executeScript({
-  //         file: 'js/script.js'
-  //       });
 	}
 });
